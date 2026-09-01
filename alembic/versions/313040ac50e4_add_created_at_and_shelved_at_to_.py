966e39ef86151d5ac18c109757f0d13148d70551
pureblue26 <2791641729@qq.com>
2026-09-01 15:30:39 +0800

    style: 迁移文件导入清理

--- alembic/versions/313040ac50e4_add_created_at_and_shelved_at_to_.py
+++ alembic/versions/313040ac50e4_add_created_at_and_shelved_at_to_.py
@@ -5,17 +5,17 @@ Revises: eb30bbac77fc
 Create Date: 2026-09-01 15:29:21.986869
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '313040ac50e4'
-down_revision: Union[str, Sequence[str], None] = 'eb30bbac77fc'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = 'eb30bbac77fc'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
